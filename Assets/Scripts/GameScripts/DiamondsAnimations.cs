2888e6dbf7bfa335123a0494da60ef2b90a2b0f0
Jakub Bodzek <jakubbodzek03@gmail.com>
2022-02-01 22:30:46 +0100

    Added diamonds, and small project fixes

--- Assets/Scripts/GameScripts/DiamondsAnimations.cs
+++ Assets/Scripts/GameScripts/DiamondsAnimations.cs
@@ -8,7 +8,7 @@ public class DiamondsAnimations : MonoBehaviour
 {
 
     // d1 stands for diamond1 etc...
-    [SerializeField] private Transform d1, d2, d3;
+    [SerializeField] private Transform d1, d2, d3, d4;
     [SerializeField] private float animDuration;
 
     private void Start()
@@ -16,5 +16,6 @@ public class DiamondsAnimations : MonoBehaviour
         d1.DOMoveY(2.8f, animDuration).SetLoops(-1, LoopType.Yoyo);
         d2.DOMoveY(7.8f, animDuration).SetLoops(-1, LoopType.Yoyo);
         d3.DOMoveY(24.8f, animDuration).SetLoops(-1, LoopType.Yoyo);
+        d4.DOMoveY(24.8f, animDuration).SetLoops(-1, LoopType.Yoyo);
     }
 }
