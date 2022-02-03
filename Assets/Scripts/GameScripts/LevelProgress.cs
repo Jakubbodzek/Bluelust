fcd1212aa6ada7b6b7943bc968287d60556b0c6d
Jakub Bodzek <jakubbodzek03@gmail.com>
2022-02-03 12:51:42 +0100

    Added new game icons, added level progress script, added level progress images

---
+++ Assets/Scripts/GameScripts/LevelProgress.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelProgress : MonoBehaviour
+{
+    [SerializeField] private Image progressBar;
+    [SerializeField] private Transform player, portal;
+
+    private Vector3 portalPosition;
+    private float distanceToPortal;
+
+    private void Start()
+    {
+        portalPosition = portal.position;
+        distanceToPortal = PortalDistance();
+    }
+
+    private float PortalDistance()
+    {
+        //return Vector3.Distance (player.position, portalPosition) ;
+        return (portalPosition - player.position).sqrMagnitude;
+    }
+
+
+    private void UpdateProgressBar(float value)
+    {
+        progressBar.fillAmount = value;
+    }
+
+    private void Update()
+    {
+        if (player.position.y <= portalPosition.y)
+        {
+            float newDistance = PortalDistance();
+            float progressValue = Mathf.InverseLerp(distanceToPortal, 0f, newDistance);
+
+            UpdateProgressBar(progressValue);
+        }
+    }
+
+}
