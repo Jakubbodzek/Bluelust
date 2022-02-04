3c3bc61c8fc8115fe853ad397ebd5daa0d106dfd
Jakub Bodzek <jakubbodzek03@gmail.com>
2022-02-04 17:43:57 +0100

    Exit panel fix

--- Assets/Scripts/MenuScripts/ExitPopup.cs
+++ Assets/Scripts/MenuScripts/ExitPopup.cs
@@ -8,18 +8,15 @@ public class ExitPopup : MonoBehaviour
 {
 
     [SerializeField] private RectTransform exitPopup;
-
-    [SerializeField] private Vector3 exitPositionFirst, exitPositionSecond;
     public void OpenPopupPanel()
     {
-        exitPopup.DOMove(exitPositionFirst, .4f);
+        exitPopup.DOAnchorPos(new Vector2(0, 0), .4f);
         exitPopup.DOScale(1f, .4f);
-        
     }
     public void ExitPopupPanel()
     {
         exitPopup.DOScale(0f, .4f);
-        exitPopup.DOMove(exitPositionSecond, .4f);
+        exitPopup.DOAnchorPos(new Vector2(0, -300), .4f);
         //exitPopup.DOScale(new Vector3(0f, 0f, 1f), .4f);
     }
 }
