a664c5d0992e4fe748810232ea44a2a819810a9a
Jakub Bodzek <jakubbodzek03@gmail.com>
2022-02-04 17:35:11 +0100

    Fixed exit popup and game hints

--- Assets/Scripts/MenuScripts/ExitPopup.cs
+++ Assets/Scripts/MenuScripts/ExitPopup.cs
@@ -8,13 +8,18 @@ public class ExitPopup : MonoBehaviour
 {
 
     [SerializeField] private RectTransform exitPopup;
+
+    [SerializeField] private Vector3 exitPositionFirst, exitPositionSecond;
     public void OpenPopupPanel()
     {
+        exitPopup.DOMove(exitPositionFirst, .4f);
         exitPopup.DOScale(1f, .4f);
+        
     }
     public void ExitPopupPanel()
     {
         exitPopup.DOScale(0f, .4f);
+        exitPopup.DOMove(exitPositionSecond, .4f);
         //exitPopup.DOScale(new Vector3(0f, 0f, 1f), .4f);
     }
 }
--- Assets/Scripts/MenuScripts/GameHints.cs
+++ Assets/Scripts/MenuScripts/GameHints.cs
@@ -20,20 +20,19 @@ public class GameHints : MonoBehaviour
         {
             generateHints = true;
             StartCoroutine(Hints());
-            hintDisplay.GetComponent<TextMeshProUGUI>().DOFade(0f, 10f).SetLoops(-1);
         }
     }
 
     IEnumerator Hints()
     {
-        hints = Random.Range(1, 6);
+        hints = Random.Range(1, 10);
         if(hints == 1)
         {
             hintDisplay.GetComponent<TextMeshProUGUI>().text = "Remember to finish the tutorial before starting!";
         }
         if (hints == 2)
         {
-            hintDisplay.GetComponent<TextMeshProUGUI>().text = "Diamonds are called Blue Lust here!";
+            hintDisplay.GetComponent<TextMeshProUGUI>().text = "Diamonds are called Lust.";
         }
         if (hints == 3)
         {
@@ -49,9 +48,25 @@ public class GameHints : MonoBehaviour
         }
         if (hints == 6)
         {
-            hintDisplay.GetComponent<TextMeshProUGUI>().text = "Complete the missions to unlock new player skins!";
+            hintDisplay.GetComponent<TextMeshProUGUI>().text = "Complete the missions to earn more Lust!";
         }
-        yield return new WaitForSeconds(10);
+        if (hints == 7)
+        {
+            hintDisplay.GetComponent<TextMeshProUGUI>().text = "Finish the tutorial to unlock a new skin!";
+        }
+        if (hints == 8)
+        {
+            hintDisplay.GetComponent<TextMeshProUGUI>().text = "You can tilt your phone to adjust the screen.";
+        }
+        if (hints == 9)
+        {
+            hintDisplay.GetComponent<TextMeshProUGUI>().text = "Explore the levels and earn more Lust!";
+        }
+        if (hints == 10)
+        {
+            hintDisplay.GetComponent<TextMeshProUGUI>().text = "If you found a bug please report it!";
+        }
+        yield return new WaitForSeconds(7);
         generateHints = false;
     }
 }
